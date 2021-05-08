5fbad35885821bcb77749c19581b4f732637ac8d
Felipe P Covatti <felipepcovatti@gmail.com>
2021-05-08 14:07:20 -0300

    Redirect from dashboard to login if without token

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import { useAuth } from "../contexts/AuthContext"
 import { api } from "../services/api"
+import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
   const { user } = useAuth()
@@ -16,3 +17,10 @@ export default function Dashboard() {
     <h1>Dasboard: {user?.email}</h1>
   )
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+
+  return {
+    props: {}
+  }
+})
